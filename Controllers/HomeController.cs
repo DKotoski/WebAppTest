99bf9d6c23621e73f1ed3e6a2eb89d8fb2147062
Darko Kotoski <wbountyhunter@hotmail.com>
2015-09-07 14:31:00 +0200

    Finished Grade Editing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,10 +9,25 @@ namespace WebAppTest.Controllers
 {
     public class HomeController : Controller
     {
+        private WebAppTestContext db = new WebAppTestContext();
         public ActionResult Index()
         {
             var data = new MasterDAO();
             return View(data);
+        }
+
+        public JsonResult EditGrade(int studentId, int subjectId, int grade)
+        {
+            var student = db.Students.Find(studentId);
+            if (student.SubjectsGrades.ContainsKey(subjectId))
+            {
+                student.SubjectsGrades[subjectId] = grade;
+            }
+            else
+            {
+                student.SubjectsGrades.Add(subjectId, grade);
+            }
+            return Json("good");
         }
     }
 }
---
+++ Scripts/CellEdit.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="jquery-1.10.2.js" />
+/// <reference path="c:\users\wboun\documents\visual studio 2015\Projects\WebAppTest\WebAppTest\Views/Home/Index.cshtml" />
+
+$(function () {
+    $(".editable").on("click", function () {
+        var text = $(this).text();
+        $(this).html("<input type='text' value="+text+" class='editableInput'>")
+    });
+    $(".editableInput").on("change", function () {
+        var val = $(this).val();
+        var parent = $(this).parent();
+        var studentID = $(parent).attr("stId");
+        var subjectID = $(parent).attr("subId");        
+        $.post("/Home/EditGrade", { studentId: studentID, subjectId: subjectID, grade: val });
+        $(parent).html(val);
+    });
+});
